2463efe17106f80bdf5fcac5eedfa65d8fedc1d7
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-07-26 00:28:09 -0300

    create interface

---
+++ apirest/src/main/java/com/produto/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.produto.apirest.repository;
+
+import com.produto.apirest.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
